0b3a077ac4f71d17af29c6aeac09fe54aebe66e9
zipitio <62092328+zipitio@users.noreply.github.com>
2024-10-28 11:06:51 -0400

    Initial Commit

---
+++ HackerTest/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  HackerTest
+//
+//  Created by Carlos Rivera on 10/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: Any) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+
+    }
+    
+    func changeColor() -> UIColor{
+
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
+
+}
+
